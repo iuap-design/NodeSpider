f8a18da4d9629f112f1c5c334c367f723d69e3e6
huyueb <1062887235@qq.com>
2017-11-03 18:29:11 +0800

    feat: 增加批量增加和更新功能

--- spider.js
+++ spider.js
@@ -1,191 +1,180 @@
 //await
 
-var http = require('http');
-var fs = require('fs');
-var cheerio = require('cheerio');
-var request = require('request');
+var http = require("http");
+var fs = require("fs");
+var cheerio = require("cheerio");
+var request = require("request");
 var concurrencyCount = 0;
-var i = 0;
-var data = [];//获取解析到的version和链接下载地址的对象，方便到时候获取到版本所对应的地址
-var url = "http://www.bootcdn.cn/react/";
-var mkdirs = require('jm-mkdirs');
+var json = "./data.json";
+// var url = "http://www.bootcdn.cn/react/";
+var mkdirs = require("jm-mkdirs");
+var json_urls = JSON.parse(fs.readFileSync(json, "utf8")).urls;
+
 //初始url
+var fetchPage = function(url) {
+  //封装了一层函数
+  var DIRNAME = url.split("/")[url.split("/").length - 2].toLowerCase();
+  if (!fs.existsSync("./data/")) {
+    fs.mkdirSync("./data/");
+  }
+  if (!fs.existsSync("./jsonconfig/")) {
+    fs.mkdirSync("./jsonconfig/");
+  }
 
-function fetchPage(x) { //封装了一层函数
-    startRequest(x);
-}
+  if (!fs.existsSync("./data/" + DIRNAME + "/")) {
+    fs.mkdirSync("./data/" + DIRNAME + "/");
+  }
+  startRequest(url, DIRNAME);
+};
 
-var DIRNAME = url.split('/')[url.split('/').length - 2].toLowerCase();
-
-if (fs.existsSync('./data/')) {
-    // console.log('已经创建过此更新目录了');
-} else {
-    fs.mkdirSync('./data/');
-    console.log('data目录已创建成功\n');
-}
-if (fs.existsSync('./jsonconfig/')) {
-    // console.log('已经创建过此更新目录了');
-} else {
-    fs.mkdirSync('./jsonconfig/');
-    console.log('jsonconfig目录已创建成功\n');
-}
-
-if (fs.existsSync('./data/' + DIRNAME + "/")) {
-    // console.log('已经创建过此更新目录了');
-} else {
-    fs.mkdirSync('./data/' + DIRNAME + "/");
-    console.log('目录已创建成功\n');
-}
-
-function startRequest(x) {
-    //采用http模块向服务器发起一次get请求
-    http.get(x, function(res) {
-        var html = ''; //用来存储请求网页的整个html内容
-        var titles = [];
-        res.setEncoding('utf-8'); //防止中文乱码
-        //监听data事件，每次取一块数据
-        res.on('data', function(chunk) {
-            html += chunk;
-        });
-        //监听end事件，如果整个网页内容的html都获取完毕，就执行回调函数
-        res.on('end', function() {
-            var $ = cheerio.load(html),
-                name = url.split('/')[url.split('/').length - 2].toLowerCase(),
-                json = {},
-                versoins = [],
-                urls = []; //采用cheerio模块解析html
-
-            json = {
-                name: name,
-                download: {}
-            }
-            
-            $('.container>h3').each(function() {
-                var $this = $(this),
-                    title = $this.text().trim(),
-                    version = title.split('：')[1],
-                    fileName = [],
-                    d_json = {},
-                    url = [],
-                    obj = {};
-                var news_item = {
-                    //获取文章的标题
-                    title: title,
-                    //i是用来判断获取了多少篇文章
-                    i: i = i + 1,
-                };
-                $this.next().find('.library-url').each(function() {
-                    var link = $(this).html(),
-                        link_arry = link.split(version+'/'),
-                        filename = '/' + link_arry[link_arry.length - 1];
-                    urls.push(link);
-                    url.push(link);
-                    fileName.push(filename);
-                })
-                json['download'][version] = fileName;
-                versoins.push(version);
-                obj["version"] = version;
-                obj["url"] = url;
-                data.push(obj);
-                if (i > 20) {
-                    return false;
-                }
-                // console.log(version)
-                // asyncFun(version);
-            });
-            console.log('通过async/awite 来实现');
-            var readFileFunPromise = function(fileName) {
-                return new Promise(function(resolve, reject) {
-                    var link = "http://" + fileName.split('://')[1];
-                    request(link, function(error, response, html) {
-                        var $ = cheerio.load(html); //采用cheerio模块解析html
-                        // callback(undefined, html);
-                        resolve(html);
-                    });
-                });
-            };
-            
-            var asyncFun = async function() {
-                for (var index = 0; index < data.length; index++) {
-                    var urls = data[index].url,
-                        version = data[index].version;
-            
-                    for (var i = 0; i < urls.length; i++) {
-                        var link = "http://" + urls[i].split('://')[1];
-                        var fileData = await readFileFunPromise(urls[i]);
-                        console.log(link);
-                        var array = link.split(version+'/');
-                        var name = array[array.length - 1];
-                        var name_array = name.split('/');
-                        var dir_name;
-                        try {
-                            if(name_array.length>1){
-                                name = name_array.pop();
-                                dir_name = name_array.join('/');
-                                var source = './data/' + DIRNAME + "/" + version +"/"+dir_name+"/";
-                                if (!fs.existsSync(source)) {
-                                    mkdirs.sync(source)
-                                } else {
-                                    
-                                }
-                                fs.writeFileSync(source + name, fileData, 'utf-8', function(err) {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-                                });
-                                
-                            }else{
-                                dir_name = array[1];
-                                var source = './data/' + DIRNAME + "/" + version;
-                                if (fs.existsSync(source)) {
-                                    // console.log('已经创建过此更新目录了');
-                                } else {
-                                    fs.mkdirSync(source);
-                                }
-                                fs.writeFileSync(source + "/" + name, fileData, 'utf-8', function(err) {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-                                });
-                            }
-                        } catch (error) {
-                            // console.log(error)
-                        }
-                        
-                        
-                    }
-                }
-                
-            };
-            asyncFun()
-            json['version'] = versoins;
-
-            if (fs.existsSync('./jsonconfig/' + name + "/")) {
-                // console.log('已经创建过此更新目录了');
-            } else {
-                fs.mkdirSync('./jsonconfig/' + name + "/");
-                console.log('jsonconfig更新目录已创建成功\n');
-            }
-            fs.writeFile('./jsonconfig/' + name + "/index.json", JSON.stringify(json), 'utf-8', function(err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        });
-    }).on('error', function(err) {
-        console.log(err);
+var getHtml = function(url) {
+  return new Promise(function(resolve, reject) {
+    request(url, function(error, response, html) {
+      var $ = cheerio.load(html); //采用cheerio模块解析html
+      // callback(undefined, html);
+      resolve(html);
     });
-}
+  });
+};
 
-function savedContent(link) {
-    link = "http://" + link.split('://')[1];
-    request(link, function(error, response, html) {
+var startRequest = async function(url, DIRNAME) {
+  var data = [],
+    i = 0,
+    //采用http模块向服务器发起一次get请求
+    html = await getHtml(url),
+    $ = cheerio.load(html),
+    name = url.split("/")[url.split("/").length - 2].toLowerCase(),
+    json = {},
+    versoins = [],
+    urls = []; //采用cheerio模块解析html
+
+  json = {
+    name: name,
+    download: {}
+  };
+
+  $(".container>h3").each(function() {
+    var $this = $(this),
+      title = $this.text().trim(),
+      version = title.split("：")[1],
+      fileName = [],
+      d_json = {},
+      url = [],
+      obj = {};
+    var news_item = {
+      //获取文章的标题
+      title: title,
+      //i是用来判断获取了多少篇文章
+      i: (i = i + 1)
+    };
+    $this
+      .next()
+      .find(".library-url")
+      .each(function() {
+        var link = $(this).html(),
+          link_arry = link.split(version + "/"),
+          filename = "/" + link_arry[link_arry.length - 1];
+        urls.push(link);
+        url.push(link);
+        fileName.push(filename);
+      });
+    json["download"][version] = fileName;
+    versoins.push(version);
+    obj["version"] = version;
+    obj["url"] = url;
+    data.push(obj);
+    if (i > 1) {
+      return false;
+    }
+  });
+  var readFileFunPromise = function(fileName) {
+    return new Promise(function(resolve, reject) {
+      var link = "http://" + fileName.split("://")[1];
+      request(link, function(error, response, html) {
         var $ = cheerio.load(html); //采用cheerio模块解析html
         // callback(undefined, html);
-
+        resolve(html);
+      });
     });
+  };
 
+  var asyncFun = async function() {
+    for (var index = 0; index < data.length; index++) {
+      var urls = data[index].url,
+        version = data[index].version;
 
+      for (var i = 0; i < urls.length; i++) {
+        var link = "http://" + urls[i].split("://")[1];
+        var fileData = await readFileFunPromise(urls[i]);
+        console.log(link);
+        var array = link.split(version + "/");
+        var name = array[array.length - 1];
+        var name_array = name.split("/");
+        var dir_name;
+        try {
+          if (name_array.length > 1) {
+            name = name_array.pop();
+            dir_name = name_array.join("/");
+            var source =
+              "./data/" + DIRNAME + "/" + version + "/" + dir_name + "/";
+            if (!fs.existsSync(source)) {
+              mkdirs.sync(source);
+            } else {
+            }
+            fs.writeFileSync(source + name, fileData, "utf-8", function(err) {
+              if (err) {
+                console.log(err);
+              }
+            });
+          } else {
+            dir_name = array[1];
+            var source = "./data/" + DIRNAME + "/" + version;
+            if (fs.existsSync(source)) {
+              // console.log('已经创建过此更新目录了');
+            } else {
+              fs.mkdirSync(source);
+            }
+            fs.writeFileSync(source + "/" + name, fileData, "utf-8", function(
+              err
+            ) {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+        } catch (error) {
+          // console.log(error)
+        }
+      }
+    }
+  };
+  asyncFun();
+  json["version"] = versoins;
+
+  if (fs.existsSync("./jsonconfig/" + name + "/")) {
+    // console.log('已经创建过此更新目录了');
+  } else {
+    fs.mkdirSync("./jsonconfig/" + name + "/");
+    console.log("jsonconfig更新目录已创建成功\n");
+  }
+  fs.writeFileSync(
+    "./jsonconfig/" + name + "/index.json",
+    JSON.stringify(json),
+    "utf-8",
+    function(err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
+
+function savedContent(link) {
+  link = "http://" + link.split("://")[1];
+  request(link, function(error, response, html) {
+    var $ = cheerio.load(html); //采用cheerio模块解析html
+    // callback(undefined, html);
+  });
 }
 
 //该函数的作用：在本地存储所爬取到的图片资源
@@ -224,4 +213,11 @@ function savedContent(link) {
 //
 // }
 // openImage(url);
-fetchPage(url); //主程序开始运行
+// fetchPage(url); //主程序开始运行
+var getUrl = function(json_urls) {
+  for (var index = 0; index < json_urls.length; index++) {
+    var url = json_urls[index];
+    fetchPage(url);
+  }
+};
+getUrl(json_urls);
